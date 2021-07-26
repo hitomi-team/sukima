2b32288e2e20a98b9df708c9e7b298db6ce971d1
Tony Mercurio <seizou.genkei@protonmail.com>
2021-07-26 07:56:27 -0700

    Implement user authentication

--- src/api.py
+++ src/api.py
@@ -3,37 +3,74 @@ from gptauto import GPTAuto
 from gptneo import GPTNeo
 from util import Util
 from functools import wraps
+from auth import Auth
 
 class API:
     def __init__(self, version='v1', app=None, config=None):
         self.app = app
         self.version = version
         self.config = config
+        if config["auth_enable"]:
+            self.auth = Auth(config)
         self.models = []
     
-    # Auth
-    def verify_token(f):
+    # Auth and Administration
+    def verify_admin_token(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             self = args[0]
             token = None
-
-            if self.config["enable_auth"] == True:
+            if self.config["auth_enable"] == True:
                 if 'Authorization' in request.headers:
                     token = request.headers['Authorization']
                 else:
                     return Util.error(None, "No token header")
-                if token != self.config["master_token"]:
+                if token != self.config["auth_admin_token"]:
                     return Util.error(None, "Invalid token")
-            
             return f(*args, **kwargs)
         return decorated_function
+
+    def verify_user_token(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            self = args[0]
+            token = None
+            if self.config["auth_enable"] == True:
+                if 'Authorization' in request.headers:
+                    token = request.headers['Authorization']
+                else:
+                    return Util.error(None, "No token header")
+                if (self.auth.auth_key(token) == False) and (token != self.config["auth_admin_token"]):
+                    return Util.error(None, "Invalid token")
+            return f(*args, **kwargs)
+        return decorated_function
+
+    # /v1/create_key
+    @verify_admin_token
+    def create_key(self):
+        if self.config["auth_enable"] == False:
+            return Util.error(None, "Authentication disabled")
+        return jsonify(key_dict = {
+            "key": self.auth.create_key()
+        })
+    
+    # /v1/delete_key
+    @verify_admin_token
+    def delete_key(self):
+        request_body = request.json
+        if request_body == None:
+            return Util.error(None, "No request body")
+        if self.config["auth_enable"] == False:
+            return Util.error(None, "Authentication disabled")
+        
+        self.auth.delete_key(request_body["key"])
+        return Util.success("Key deleted")
 
     # API Functions
 
     # Get models
     # /v1/models
-    @verify_token
+    @verify_user_token
     def get_model_list(self):
         model_dict = {
             "models": {
@@ -47,7 +84,7 @@ class API:
     
     # Load model
     # /v1/load
-    @verify_token
+    @verify_user_token
     def load_model(self):
         request_body = request.json
         if request_body == None:
@@ -69,7 +106,7 @@ class API:
     
     # Delete model
     # /v1/delete
-    @verify_token
+    @verify_user_token
     def delete_model(self):
         request_body = request.json
         if request_body == None:
@@ -84,7 +121,7 @@ class API:
 
     # Generate from model
     # /v1/generate
-    @verify_token
+    @verify_user_token
     def generate(self):
         request_body = request.json
         if request_body == None:
@@ -104,6 +141,8 @@ class API:
         return Util.error(None, "Model not found")
     
     def run(self):
+        self.app.add_url_rule('/{}/create_key'.format(self.version), self.create_key, methods=['GET'])
+        self.app.add_url_rule('/{}/delete_key'.format(self.version), self.delete_key, methods=['POST'])
         self.app.add_url_rule('/{}/models'.format(self.version), view_func=self.get_model_list, methods=['GET'])
         self.app.add_url_rule('/{}/generate'.format(self.version), view_func=self.generate, methods=['POST'])
         self.app.add_url_rule('/{}/load'.format(self.version), view_func=self.load_model, methods=['POST'])
