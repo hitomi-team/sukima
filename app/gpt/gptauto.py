e29cad5dc44b0175179091dce30ff3fc494aa25d
Tony <seizou.genkei@protonmail.com>
2021-12-26 08:12:05 +0900

    Make GPTAuto consistent

--- app/gpt/gptauto.py
+++ app/gpt/gptauto.py
@@ -2,5 +2,5 @@ class GPTAuto:
     def __init__(self, model_name='generic'):
         self.model_name = model_name
 
-    def generate(self, prompt, args):
-        return prompt
+    def generate(self, args):
+        return 'Not implemented'
