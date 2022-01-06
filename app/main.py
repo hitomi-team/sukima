8983769e3cf0299ad110efb9e1c3eed7425904a0
Cielo <51804240+zielo-hue@users.noreply.github.com>
2022-01-05 21:42:31 -0800

    Rayxperimentation begins

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import ray
+from ray import serve
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -7,6 +9,8 @@ from app.api.v1.api import api_router
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
+ray.init(address="auto", namespace="raytest")
+serve.start(detached=True)
 
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
@@ -19,6 +23,16 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(api_router, prefix="/api/v1", tags=["v1"])
 
+
 @app.get("/")
 async def root():
     return 'Sometimes I dream about cheese.'
+
+
+# Ray cluster launcher wen
+@serve.deployment(route_prefix="/raytest", num_replicas=2)
+@serve.ingress(app)
+class FAPIWrapper:
+    @app.get("/")
+    def root(self):
+        return "I hate cheese!"
